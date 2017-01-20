d576e10567fae911488f5d136ac394793d4aae16
Moises Alvarado <moises.alvarado.sme@outlook.com>
2017-01-20 09:40:17 -0400

    termino de clase 15

--- Segundo proyecto - Dibujando con Canvas/canvas/dibujo.js
+++ Segundo proyecto - Dibujando con Canvas/canvas/dibujo.js
@@ -12,10 +12,33 @@ function dibujar(color, posIniX, posIniY, posFinX, posFinY) {
   //Cerrar el trazo:
   lienzo.closePath();
 }
+function dibujoPorClick() {
+  var valor = parseInt(texto.value);
+  var aux = anchoCanvas/valor;
+
+  for (var i = 0; i < valor; i++) {
+    //Inferior izquierdo:
+    dibujar(color_, 0, i*aux, aux*(i+1), 300);
+    //Inferior derecho:
+    dibujar(color_, i*aux, 0, 300, aux*(i+1));
+  }
+
+  //Borde inferior izquierdo:
+  dibujar(color_, 1,0,1,anchoCanvas-1);
+  dibujar(color_, 0,anchoCanvas-1,anchoCanvas-1,anchoCanvas-1);
+  //Borde superior derecho:
+  dibujar(color_, 1,0,anchoCanvas-1,1);
+  dibujar(color_, anchoCanvas-1,1,anchoCanvas-1,anchoCanvas-1);
+
+}
 
 var d = document.getElementById("canvasDibujo");
+var anchoCanvas = d.width;
 var lienzo = d.getContext("2d");
+var color_ = "#FAA";
 
-dibujar("red", 100, 10, 200, 100);
-dibujar("black", 120, 10, 250, 100);
-dibujar("green", 140, 10, 299, 100);
+var texto = document.getElementById("cantidad");
+var botoncito = document.getElementById("boton");
+
+//Eventos:
+botoncito.addEventListener("click", dibujoPorClick);
